e1337f1b89264b7d8486019e5cb8616dea5bf81d
Michael K. Edwards <m.k.edwards@gmail.com>
2019-02-11 23:20:57 +0000

    turn flaky unclean leader election tests back off

--- core/src/test/scala/unit/kafka/integration/UncleanLeaderElectionTest.scala
+++ core/src/test/scala/unit/kafka/integration/UncleanLeaderElectionTest.scala
@@ -114,6 +114,7 @@ class UncleanLeaderElectionTest extends ZooKeeperTestHarness {
   }
 
   @Test
+  @Ignore
   def testUncleanLeaderElectionDisabled(): Unit = {
     // unclean leader election is disabled by default
     startBrokers(Seq(configProps1, configProps2))
@@ -141,6 +142,7 @@ class UncleanLeaderElectionTest extends ZooKeeperTestHarness {
   }
 
   @Test
+  @Ignore
   def testUncleanLeaderElectionDisabledByTopicOverride(): Unit = {
     // enable unclean leader election globally, but disable for our specific test topic
     configProps1.put("unclean.leader.election.enable", "true")
