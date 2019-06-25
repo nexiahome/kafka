2440efb94b62bcd4c7d5cc2066e2d3dcf490352d
Michael Edwards <medwards@bitpusher.com>
2019-06-25 14:56:00 -0700

    per-partition metrics for LoadGroupsAndOffsets

--- core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala
+++ core/src/main/scala/kafka/coordinator/group/GroupMetadataManager.scala
@@ -499,9 +499,17 @@ class GroupMetadataManager(brokerId: Int,
       }
 
       try {
+        val tags:Map[String,String] = Map("topic" -> Topic.GROUP_METADATA_TOPIC_NAME, "partition" -> offsetsPartition.toString)
+        removeMetric("LoadGroupsAndOffsetsDurationMs", tags)
         val startMs = time.milliseconds()
         loadGroupsAndOffsets(topicPartition, onGroupLoaded)
-        info(s"Finished loading offsets and group metadata from $topicPartition in ${time.milliseconds() - startMs} milliseconds.")
+        val durationMs = time.milliseconds() - startMs
+        info(s"Finished loading offsets and group metadata from $topicPartition in $durationMs milliseconds.")
+        newGauge("LoadGroupsAndOffsetsDurationMs",
+          new Gauge[Long] {
+            def getValue() = durationMs
+          },
+          tags)
       } catch {
         case t: Throwable => error(s"Error loading offsets from $topicPartition", t)
       } finally {
@@ -713,6 +721,8 @@ class GroupMetadataManager(brokerId: Int,
         }
       }
 
+      val tags:Map[String,String] = Map("topic" -> Topic.GROUP_METADATA_TOPIC_NAME, "partition" -> offsetsPartition.toString)
+      removeMetric("LoadGroupsAndOffsetsDurationMs", tags)
       info(s"Finished unloading $topicPartition. Removed $numOffsetsRemoved cached offsets " +
         s"and $numGroupsRemoved cached groups.")
     }
