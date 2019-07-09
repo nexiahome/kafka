e3dd28d66485c7dcc1dbd89d27af7a9701f2c34b
Michael Edwards <medwards@bitpusher.com>
2019-07-09 06:00:45 -0700

    bump to 1.1.1-nexia8

--- tests/kafkatest/version.py
+++ tests/kafkatest/version.py
@@ -60,7 +60,7 @@ def get_version(node=None):
         return DEV_BRANCH
 
 DEV_BRANCH = KafkaVersion("dev")
-DEV_VERSION = KafkaVersion("1.1.1-nexia7")
+DEV_VERSION = KafkaVersion("1.1.1-nexia8")
 
 # 0.8.2.X versions
 V_0_8_2_1 = KafkaVersion("0.8.2.1")
