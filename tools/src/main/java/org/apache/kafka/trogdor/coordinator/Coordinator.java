d682f6cd1f28799c9cc2d0fbe3934eb056ffa2f0
Eric Schwartz <eric.schwartz@irco.com>
2020-01-09 13:36:33 -0700

    Update dependencies, customize build target to nexia
    forward-port of ef20648

--- tools/src/main/java/org/apache/kafka/trogdor/coordinator/Coordinator.java
+++ tools/src/main/java/org/apache/kafka/trogdor/coordinator/Coordinator.java
@@ -46,7 +46,7 @@ import static net.sourceforge.argparse4j.impl.Arguments.store;
 /**
  * The Trogdor coordinator.
  *
- * The coordinator manages the agent processes in the cluster. 
+ * The coordinator manages the agent processes in the cluster.
  */
 public final class Coordinator {
     private static final Logger log = LoggerFactory.getLogger(Coordinator.class);
@@ -131,7 +131,8 @@ public final class Coordinator {
 
     public static void main(String[] args) throws Exception {
         ArgumentParser parser = ArgumentParsers
-            .newArgumentParser("trogdor-coordinator")
+            .newFor("trogdor-coordinator")
+            .build()
             .defaultHelp(true)
             .description("The Trogdor fault injection coordinator");
         parser.addArgument("--coordinator.config", "-c")
